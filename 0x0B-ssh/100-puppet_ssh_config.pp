c9fc7d18c22b5b2532b1dd7c7fd5cae4be9522a2
254guru <otienooluda@gmail.com>
2023-11-26 12:17:27 -0800

    update config using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-#using puppet to make changes to config file
+# using puppet to make changes to config file
 
 file { '/etc/ssh/ssh_config':
 	ensure => present,
 
 content =>"
 
+	# client config
 	host*
 	IdentifyFile ~/.ssh/school
 	PasswordAuthentication no
