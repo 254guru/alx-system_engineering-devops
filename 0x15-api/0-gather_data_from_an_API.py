58944c8997791674583ad41f2bd4899b45317863
254guru <otienooluda@gmail.com>
2024-01-02 07:38:02 -0800

    update script to meet style requirements

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,38 +7,25 @@ import requests
 import sys
 
 
-def fetch_employee_todo_progress(employee_id):
-    """
-    function that crawls the API and returns the employee progress
-    """
-    base_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-    response = requests.get(base_url)
-
-    if response.status_code == 200:
-        todos = response.json()
-
-        # Fetching user data separately to get the user's name
-        user_res = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
-        if user_res.status_code == 200:
-            user_data = user_res.json()
-            employee_name = user_data['name']
-        else:
-            print(f"Failed to fetch user data. Status code: {user_res.status_code}")
-            return
-
-        completed_tasks = [task['title'] for task in todos if task['completed']]
-        total_tasks = len(todos)
-
-        print(f"Employee {employee_name} is done with tasks ({len(completed_tasks)}/{total_tasks}):")
-        for task in completed_tasks:
-            print(f"\t{task}")
-    else:
-        print(f"Failed to fetch data. Status code: {response.status_code}")
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script_name.py <employee_id>")
-    else:
-        employee_id = sys.argv[1]
-        fetch_employee_todo_progress(employee_id)
+    """
+    crawls the API and returns the employee progress
+    """
+    user_id = sys.argv[1]
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    user_str = '{}users/{}'.format(base_url, user_id)
+    todos_str = '{}todos?userId={}'.format(base_url, user_id)
+    employee_str = "Employee {} is done with tasks"
+
+    res = requests.get(user_str)
+    print(employee_str.format(res.json().get('name')), end="")
+
+    res = requests.get(todos_str)
+    tasks = []
+    for task in res.json():
+        if task.get('completed') is True:
+            tasks.append(task)
+
+    print("({}/{}):".format(len(tasks), len(res.json())))
+    for task in tasks:
+        print("\t {}".format(task.get("title")))
