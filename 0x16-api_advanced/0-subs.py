d66ab5c7bcda3d852a4d824d73c237ef875fb489
254guru <otienooluda@gmail.com>
2024-01-16 10:54:12 -0800

    subscriber count

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,14 +13,11 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'CustomUserAgent/1.0'}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
-    response = request.get(url, headers=headers)
+    response = request.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
         subscribers_count = data['data']['subscribers']
         return subscribers_count
-    elif response.status_code == 404:
-        return 0
-    else:
-        print(f"Error: {response.status_code}")
+    else response.status_code == 404:
         return 0
