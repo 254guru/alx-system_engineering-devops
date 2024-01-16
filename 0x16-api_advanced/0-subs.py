8d7b8b45167e9598565deb3d936269e989264a58
254guru <otienooluda@gmail.com>
2024-01-16 09:45:14 -0800

    get subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,24 +13,20 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'CustomUserAgent/1.0'}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
-    try:
-        response = request.get(url, headers=headers)
-        response.raise_for_status() # raises stored HTTPError, if one occured
-    except requests.exceptions.HTTPError as errh:
-        print ("Http Error:",errh)
+    response = request.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers_count = data['data']['subscribers']
+        return subscribers_count
+    elif response.status_code == 404:
         return 0
-    except requests.exceptions.ConnectionError as errc:
-        print ("Error Connecting:",errc)
-        return 0
-    except requests.exceptions.Timeout as errt:
-        print ("Timeout Error:",errt)
-        return 0
-    except requests.exceptions.RequestException as err:
-        print ("Something went wrong", err)
+    else:
+        print(f"Error: {response.status_code}")
         return 0
 
-    json_data =response.json()
-    if 'data' in json_data and 'subscribers' in json_data['data']:
-        return int(json_data['data']['subscribers'])
-    else:
-        return 0
+
+if __name__ == '__main__':
+    subreddit_name = input("Enter the subreddit name: ")
+    result = number_of_subscribers(subreddit_name)
+    print(f"Number of subscribers: {result}")
