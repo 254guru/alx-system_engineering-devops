e7f14ec5d38f88dc8176867db6ebd32cc9076e6d
254guru <otienooluda@gmail.com>
2024-01-17 13:38:53 -0800

    print top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,28 @@
+#!/user/bin/python3
+"""
+import module
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    function that queries the reddit API and prints tittles of
+    first 10 hot posts listed for a given subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'CustomUserAgent/1.0'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+
+        for post in posts:
+            print(post['data']['title'])
+    elif response.status_code == 404:
+        print(None)
+    else:
+        print(f"Error: {response.status_code}")
+        print(None)
